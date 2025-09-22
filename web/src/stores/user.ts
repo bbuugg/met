9d607b2255d027fc40692eaf383fcc7830eac91d
chengyao <chengyao0320@foxmail.com>
2025-09-22 22:05:24 +0800

    refactor

--- web/src/stores/user.ts
+++ web/src/stores/user.ts
@@ -1,13 +1,13 @@
-import { ref } from 'vue'
-import { defineStore } from 'pinia'
-import type { User } from '@/types/user'
 import { getUserInfo } from '@/api'
+import type { User } from '@/types/user'
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
 
 export const useUserStore = defineStore('user', () => {
   const info = ref<User>({} as User)
   const updateInfo = async () => {
     const { data } = await getUserInfo()
-    Object.assign(info.value, data)
+    info.value = data
   }
 
   return { info, updateInfo }
