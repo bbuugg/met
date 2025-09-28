82a0739b283018f9f21abc616b834e0d13a08d6d
chengyao <chengyao0320@foxmail.com>
2025-09-28 13:45:50 +0800

    update

--- server/internal/model/entity/user.go
+++ server/internal/model/entity/user.go
@@ -10,7 +10,7 @@ type User struct {
 	Id        uint           `gorm:"primarykey" json:"-"`
 	Uuid      string         `gorm:"type:char(36);uniqueIndex;not null" json:"uuid"`
 	Name      string         `gorm:"not null;size:100;charset:utf8mb4;collate:utf8mb4_unicode_ci" json:"name"`
-	Email     string         `gorm:"uniqueIndex;size:255;charset:utf8mb4;collate:utf8mb4_unicode_ci" json:"email"`
+	Email     string         `gorm:"size:255;charset:utf8mb4;collate:utf8mb4_unicode_ci" json:"email"`
 	Avatar    string         `gorm:"size:500" json:"avatar"`
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
